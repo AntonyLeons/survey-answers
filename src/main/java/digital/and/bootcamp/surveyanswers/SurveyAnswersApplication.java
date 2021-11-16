d07312960c047978e2ef28d3b8561c171fbe32ed
Joseph Owen <josephowen@Josephs-MacBook-Pro.local>
2021-11-16 14:11:27 +0000

    Project wireframe

---
+++ src/main/java/digital/and/bootcamp/surveyanswers/SurveyAnswersApplication.java
@@ -0,0 +1,13 @@
+package digital.and.bootcamp.surveyanswers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyAnswersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digital/and/bootcamp/surveyanswers/SurveyAnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.and.bootcamp.surveyanswers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAnswersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
